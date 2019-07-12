350a65dc31a6a535c46c471ef348f5748c6c2fb1
Ruben Lindström <ruben.lindstrom@gmail.com>
2019-07-12 13:50:00 +0200

    Initial commit

---
+++ js/api.js
@@ -0,0 +1,6 @@
+const API_URL =
+  "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json";
+
+const getWeatherDataToday = () => {};
+
+export { getWeatherDataToday, getWeatherDataTomorrow };
