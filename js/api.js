81c3f971e74431cff954352285d3f131737b935e
Ruben Lindström <ruben.lindstrom@gmail.com>
2019-07-16 13:34:36 +0200

    Added functionality to fetch weatherdata, as well as completing the picture tags

--- js/api.js
+++ js/api.js
@@ -1,6 +1,60 @@
-const API_URL =
-  "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json";
+// Returns an integer, representing the hour in a Thh:mm:ssZ-format
+const getHour = (time) => {
+  return parseInt((time.length == 10) ? time[1] + time[2] : time[11] + time[12]);
+}
 
-const getWeatherDataToday = () => {};
+const getWeatherData = () => {
+  return fetch(API_ENDPOINT)
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseJSON) => {
+      const returnData = {
+        "today": [],
+        "tomorrow": []
+      };
 
-export { getWeatherDataToday, getWeatherDataTomorrow };
+      // 2019-07-13T16:00:00Z
+      const startDateTime = responseJSON.timeSeries[0].validTime
+      const startDate = startDateTime.substring(0, 10);
+      const startTime = startDateTime.substring(10);
+
+      const dataPoints = [startDate + "T06:00:00Z",
+      startDate + "T12:00:00Z",
+      startDate + "T18:00:00Z"];
+
+      // DATAPOINTS TODAY
+      dataPoints.map((e) => {
+        if (e < startDateTime) {
+          returnData.today[e] = null;
+        } else {
+          returnData.today[e] = responseJSON.timeSeries[getHour(e) - getHour(startTime)];
+        }
+      });
+
+      // DATAPOINTS TOMORROW
+      for (let index = 0; index < 3; index++) {
+        // Avståndet mellan midnatt idag och kl. 06:00 imorgon är 30h. Vi subtraherar timmarna 
+        // passerade sedan dess, för att finna deras index. Därefter adderar vi multiplar av
+        // sex för att stega oss igenom tiderna
+        const dataIndex = 30 - getHour(startTime) + ((index) * 6);
+
+        const weatherData = responseJSON.timeSeries[dataIndex];
+        const dateTime = weatherData.validTime;
+
+        returnData.tomorrow[dateTime] = weatherData;
+      }
+      return returnData;
+    });
+}
+
+
+//const LON = 18.1489;
+//const LAT = 57.3081;
+
+//LÖTTORP
+const LAT = 57.1660;
+const LON = 16.9930;
+const API_ENDPOINT = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${LON}/lat/${LAT}/data.json`;
+
+export { getWeatherData };
--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
+import { getWeatherData } from './api.js';
+
+const main = () => {
+    getWeatherData()
+        .then((response) => {
+            console.dir(response);
+        });
+
+}
+
+const populateTables = (weatherData) => {
+
+
+    weatherData.today.forEach((e) => {
+
+    });
+}
+
+const createTable = (heading) => {
+    const table = document.getElementById("weather-info");
+    const firstTableRow = createElement("tr");
+    const labelContainer = createElement("th");
+    const label = createElement("h2");
+    label.text = heading;
+
+    labelContainer.appendChild(label);
+    firstTableRow.appendChild(labelContainer);
+    table.appendChild(firstTableRow);
+
+}
+
+
 window.addEventListener("load", main);
